cfde94553947125d63ee4a4f8314b2acc2d0f6c2
handuah <handuah@gmail.com>
2023-03-14 13:08:32 +0000

    set up foodcards with wrap

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List foods = [
+    "burrger",
+    "burrger",
+    "burrger",
+    "burrger",
+    "burrger",
+    "burrger"
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -22,7 +30,78 @@ class _HomeState extends State<Home> {
           Container(
             height: size.height,
             width: size.width * 2 / 3,
-            color: orangish,
+            color: Colors.white,
+            child: ListView(
+              padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.02,
+                horizontal: size.width * 0.02,
+              ),
+              scrollDirection: Axis.vertical,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Categories',
+                      style: boldText.copyWith(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.2,
+                    ),
+                    SizedBox(
+                      height: size.height * 0.15,
+                      width: size.width * 0.25,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          hintStyle: boldText.copyWith(
+                            color: blackish.withOpacity(0.6),
+                            fontSize: 12.0,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: blackish.withOpacity(0.6),
+                          ),
+                          fillColor: deepGrayish,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   height: size.height * 0.15,
+                    //   width: size.width * 0.1,
+                    //   child: IconButton(
+                    //     icon: const Icon(
+                    //       Icons.filter_list_rounded,
+                    //       color: Colors.black,
+                    //     ),
+                    //     color: orangish,
+                    //     onPressed: () {},
+                    //   ),
+                    // ),
+                  ],
+                ),
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+                Wrap(
+                  alignment: WrapAlignment.start,
+                  // runAlignment: WrapAlignment.start,
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  runSpacing: size.width * 0.02,
+                  spacing: size.width * 0.01,
+                  children: [
+                    // foodCard(context),
+                    // foodCard(context),
+                    // foodCard(context)
+                    for (var food in foods) foodCard(context, food)
+                  ],
+                ),
+              ],
+            ),
           ),
           Expanded(
             child: Container(
@@ -32,6 +111,37 @@ class _HomeState extends State<Home> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget foodCard(BuildContext context, String foodName) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.4,
+      width: size.width * 0.2,
+      child: Card(
+        color: Colors.green,
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: size.height * 0.25,
+              width: size.width,
+              color: Colors.red,
+            ),
+            Container(
+              height: size.height * 0.1,
+              width: size.width,
+              color: Colors.green,
+              child: Text(foodName),
+            ),
+          ],
+        ),
       ),
     );
   }
