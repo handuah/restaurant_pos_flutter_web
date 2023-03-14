2879fb823a2cfd26cda59ae7dc66a25a024bfe64
handuah <handuah@gmail.com>
2023-03-14 12:02:00 +0000

    created page content divisions

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:restaurant_pos_flutter_web/Styles/style.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,8 +13,26 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text('home'),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: size.height,
+            width: size.width * 2 / 3,
+            color: orangish,
+          ),
+          Expanded(
+            child: Container(
+              height: size.height,
+              // width: size.width * 0.5 / 3,
+              color: bluish,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
