897c330f1ded7a1afca2694f7d64b68cc508ecd7
handuah <handuah@gmail.com>
2023-03-21 12:34:07 +0000

    added more foods

--- lib/fake_jsons/foods.dart
+++ lib/fake_jsons/foods.dart
@@ -1,5 +1,6 @@
 List foods = [
   {
+    "id": 1,
     "foodName": "Burger",
     "imgUrl":
         "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
@@ -8,41 +9,99 @@ List foods = [
     "price": 35.00,
   },
   {
+    "id": 2,
     "foodName": "Burger",
     "imgUrl":
-        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+        "https://www.seriouseats.com/thmb/e16lLOoVEix_JZTv7iNyAuWkPn8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__09__20140918-jamie-olivers-comfort-food-insanity-burger-david-loftus-f7d9042bdc2a468fbbd50b10d467dafd.jpg",
     "desc":
         "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
     "price": 35.00,
   },
   {
+    "id": 3,
     "foodName": "Burger",
     "imgUrl":
-        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+        "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p.jpg",
     "desc":
         "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
     "price": 35.00,
   },
   {
+    "id": 4,
     "foodName": "Burger",
     "imgUrl":
-        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+        "https://hips.hearstapps.com/hmg-prod/images/copycat-western-bacon-cheeseburger-2-1651789305.jpeg?crop=1.00xw:0.755xh;0,0.166xh&resize=1200:*",
     "desc":
         "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
     "price": 35.00,
   },
   {
+    "id": 5,
     "foodName": "Burger",
     "imgUrl":
-        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+        "https://assets.cntraveller.in/photos/6246f468c579dd6f2d1244b6/1:1/w_1591,h_1591,c_limit/Best%20Burgers%20in%20Mumbai%20_Louis%20Monstercheese%20Burger.jpg",
     "desc":
         "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
     "price": 35.00,
   },
   {
+    "id": 6,
     "foodName": "Burger",
     "imgUrl":
-        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+        "https://www.unileverfoodsolutions.co.za/dam/global-ufs/mcos/SOUTH-AFRICA/calcmenu/recipes/ZA-recipes/general/beef-burger/main-header.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 7,
+    "foodName": "Pizza",
+    "imgUrl":
+        "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 8,
+    "foodName": "Pizza",
+    "imgUrl":
+        "https://foodhub.scene7.com/is/image/woolworthsltdprod/2004-easy-pepperoni-pizza:Mobile-1300x1150",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 9,
+    "foodName": "Pizza",
+    "imgUrl": "https://i.ytimg.com/vi/XKC62IFnH2s/maxresdefault.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 10,
+    "foodName": "Pizza",
+    "imgUrl":
+        "https://coupons.com.gh//blog/assets/images/top-pizza-restaurants-in-accra.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 11,
+    "foodName": "Pizza",
+    "imgUrl":
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlKkunuuzfERtMsuLRfbe0fTuQUFQMOQ-tw&usqp=CAU",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "id": 12,
+    "foodName": "Pizza",
+    "imgUrl":
+        "https://img.kidspot.com.au/pZnR2nZu/kk/2015/03/hawaiian-pizza-recipe-605894-2.jpg",
     "desc":
         "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
     "price": 35.00,
