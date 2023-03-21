a5cba42d034150cc09d7c864caa1915263ee2a44
handuah <handuah@gmail.com>
2023-03-21 11:34:37 +0000

    init commir

--- lib/fake_jsons/foods.dart
+++ lib/fake_jsons/foods.dart
@@ -1,1 +1,50 @@
-List foods = ["burrger", "burrger", "burrger", "burrger", "burrger", "burrger"];
+List foods = [
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+  {
+    "foodName": "Burger",
+    "imgUrl":
+        "https://recipetineats.com/wp-content/uploads/2018/04/Veggie-Burgers_9a.jpg",
+    "desc":
+        "An amazing meal made with papaya, lettuce, tomatoes and no protein for subtuousness",
+    "price": 35.00,
+  },
+];
