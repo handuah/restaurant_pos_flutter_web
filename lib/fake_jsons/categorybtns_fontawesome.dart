a9189e5788d07d8d6c3ab099deb6c4527d615a76
handuah <handuah@gmail.com>
2023-03-21 10:10:07 +0000

    init commit

---
+++ lib/fake_jsons/categorybtns_fontawesome.dart
@@ -0,0 +1,10 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List categoryIcons = [
+  {"fontawesome_icon": FontAwesomeIcons.cashRegister, "categoryName": 'All'},
+  {'fontawesome_icon': FontAwesomeIcons.pizzaSlice, 'categoryName': 'Pizza'},
+  {'fontawesome_icon': FontAwesomeIcons.burger, 'categoryName': 'Burger'},
+  {'fontawesome_icon': FontAwesomeIcons.bottleWater, 'categoryName': 'Juice'},
+  {'fontawesome_icon': FontAwesomeIcons.iceCream, 'categoryName': 'Ice Cream'},
+  {'fontawesome_icon': FontAwesomeIcons.mugHot, 'categoryName': 'Coffee'}
+];
